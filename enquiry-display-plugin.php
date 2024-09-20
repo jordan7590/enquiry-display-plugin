28b2f8c8233c9b94b15c20b604ddf703516c9d19
jordan-bell <jordan.whitedigi@gmail.com>
2024-09-21 04:28:53 +0530

    aaa

--- enquiry-display-plugin.php
+++ enquiry-display-plugin.php
@@ -205,35 +205,45 @@ function ced_generate_styled_html($data, $enquiry_id) {
             </div>
         </div>
         <div class="column">
-            <div class="details-card">
-                <div class="details-header">Journey Details</div>
-                <div class="details-content">
-                   <div class="arrival">
-                   <h3>Arrival</h3>
-                    <div class="detail-item">
-                        <span class="detail-label">Date:</span>
-                        <span class="detail-value"><?php echo esc_html($data['journey_details']['arrival_details']['arrival_date']); ?></span>
-                    </div>
-                    <div class="detail-item">
-                        <span class="detail-label">Address:</span>
-                        <span class="detail-value"><?php echo esc_html($data['journey_details']['arrival_details']['arrival_address']); ?></span>
-                    </div>
-                   </div>
-                   <div class="departure">
-                   <h3>Departure</h3>
-                    <div class="detail-item">
-                        <span class="detail-label">Date:</span>
-                        <span class="detail-value"><?php echo esc_html($data['journey_details']['departure_details']['arrival_date']); ?></span>
-                    </div>
-                    <div class="detail-item">
-                        <span class="detail-label">Address:</span>
-                        <span class="detail-value"><?php echo esc_html($data['journey_details']['departure_details']['departure_address']); ?></span>
-                    </div>
-                    </div>
-                    
+    <div class="details-card">
+        <div class="details-header">Journey Details</div>
+        <div class="details-content">
+           <div class="arrival">
+               <h3>
+                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon-plane">
+                       <path d="M17.8 19.2 16 11l3.5-3.5C21 6 21.5 4 21 3c-1-.5-3 0-4.5 1.5L13 8 4.8 6.2c-.5-.1-.9.1-1.1.5l-.3.5c-.2.5-.1 1 .3 1.3L9 12l-2 3H4l-1 1 3 2 2 3 1-1v-3l3-2 3.5 5.3c.3.4.8.5 1.3.3l.5-.2c.4-.3.6-.7.5-1.2z"></path>
+                   </svg>
+                   Arrival
+               </h3>
+                <div class="detail-item">
+                    <span class="detail-label">Date:</span>
+                    <span class="detail-value"><?php echo esc_html($data['journey_details']['arrival_details']['arrival_date']); ?></span>
+                </div>
+                <div class="detail-item">
+                    <span class="detail-label">Address:</span>
+                    <span class="detail-value"><?php echo esc_html($data['journey_details']['arrival_details']['arrival_address']); ?></span>
+                </div>
+           </div>
+           <div class="departure">
+               <h3>
+                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon-home">
+                       <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                       <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                   </svg>
+                   Departure
+               </h3>
+                <div class="detail-item">
+                    <span class="detail-label">Date:</span>
+                    <span class="detail-value"><?php echo esc_html($data['journey_details']['departure_details']['departure_date']); ?></span>
+                </div>
+                <div class="detail-item">
+                    <span class="detail-label">Address:</span>
+                    <span class="detail-value"><?php echo esc_html($data['journey_details']['departure_details']['departure_address']); ?></span>
                 </div>
             </div>
         </div>
+    </div>
+</div>
     </div>
 
     <div class="details-card">
